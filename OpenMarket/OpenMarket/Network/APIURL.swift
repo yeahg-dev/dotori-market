2376133c02d82cac6e94e640cf0f430f099f88b7
Jager-yoo <y73447jh@gmail.com>
2022-01-07 18:07:37 +0900

    refactor: 기본 URL 네이밍 수정 openMarketAPIHost -> apiHost

--- OpenMarket/OpenMarket/Network/APIURL.swift
+++ OpenMarket/OpenMarket/Network/APIURL.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum APIURL {
 
-    private static let openMarketAPIHost = "https://market-training.yagom-academy.kr/"
+    private static let apiHost = "https://market-training.yagom-academy.kr/"
 
     case healthChecker
     case productRegistration
@@ -22,19 +22,19 @@ enum APIURL {
     var url: URL? {
         switch self {
         case .healthChecker:
-            return URL(string: Self.openMarketAPIHost + "healthChecker")
+            return URL(string: Self.apiHost + "healthChecker")
         case .productRegistration:
-            return URL(string: Self.openMarketAPIHost + "api/products")
+            return URL(string: Self.apiHost + "api/products")
         case .productInfoEdit(let productID):
-            return URL(string: Self.openMarketAPIHost + "api/products/\(productID)")
+            return URL(string: Self.apiHost + "api/products/\(productID)")
         case .productSecret(let productID):
-            return URL(string: Self.openMarketAPIHost + "api/products/\(productID)/secret")
+            return URL(string: Self.apiHost + "api/products/\(productID)/secret")
         case .productDeletion(let productID, let productSecret):
-            return URL(string: Self.openMarketAPIHost + "api/products/\(productID)/\(productSecret)")
+            return URL(string: Self.apiHost + "api/products/\(productID)/\(productSecret)")
         case .productDetail(let productID):
-            return URL(string: Self.openMarketAPIHost + "api/products/\(productID)")
+            return URL(string: Self.apiHost + "api/products/\(productID)")
         case .productsListPage(let pageNo, let itemsPerPage):
-            var urlComponents = URLComponents(string: Self.openMarketAPIHost + "api/products")
+            var urlComponents = URLComponents(string: Self.apiHost + "api/products")
             let pageNo = URLQueryItem(name: "page_no", value: "\(pageNo)")
             let itemsPerPage = URLQueryItem(name: "items_per_page", value: "\(itemsPerPage)")
             urlComponents?.queryItems = [pageNo, itemsPerPage]
