d8fde571ef6e6f8417d61cd19ec45ed935bb4b6e
Jager-yoo <y73447jh@gmail.com>
2022-01-13 15:36:14 +0900

    feat: Navigation Bar 위에 커스텀 Segmented Control 구현

--- OpenMarket/OpenMarket/Controller/TabBarViewController.swift
+++ OpenMarket/OpenMarket/Controller/TabBarViewController.swift
@@ -7,12 +7,30 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-
+    
+    @IBOutlet private weak var viewSegmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureViewSegmentedControl()
     }
 
-
+    private func configureViewSegmentedControl() {
+        viewSegmentedControl.setTitle("LIST", forSegmentAt: 0)
+        viewSegmentedControl.setTitle("GRID", forSegmentAt: 1)
+        viewSegmentedControl.setTitleTextAttributes(
+            [.foregroundColor: UIColor.systemBlue],
+            for: .normal
+        )
+        viewSegmentedControl.setTitleTextAttributes(
+            [.foregroundColor: UIColor.white],
+            for: .selected
+        )
+        viewSegmentedControl.selectedSegmentTintColor = .systemBlue
+        viewSegmentedControl.backgroundColor = .white
+        viewSegmentedControl.layer.borderColor = UIColor.systemBlue.cgColor
+        viewSegmentedControl.layer.borderWidth = 1
+    }
+    
 }
 
