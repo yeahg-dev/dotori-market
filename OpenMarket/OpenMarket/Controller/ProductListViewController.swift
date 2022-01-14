b504f360778a7300c873061a7382d0509d3def46
yeahg-dev <yeahg.dev@gmail.com>
2022-01-14 14:41:31 +0900

    feat: getImage 메서드 구현 및 productListViewCell의 image 세팅 코드 추가

--- OpenMarket/OpenMarket/Controller/ProductListViewController.swift
+++ OpenMarket/OpenMarket/Controller/ProductListViewController.swift
@@ -2,7 +2,7 @@
 //  ProductListViewController.swift
 //  OpenMarket
 //
-//  Created by 유재호 on 2022/01/12.
+//  Created by 예거 on 2022/01/12.
 //
 
 import UIKit
@@ -54,57 +54,20 @@ class ProductListViewController: UITableViewController {
         guard let product = initialProductsListPage?.pages[indexPath.row] else {
             return cell
         }
-        
+    
+        cell.productThumbnail.image = getImage(from: product.thumbnail)
         cell.productName.attributedText = product.attributedName
         cell.productPrice.attributedText = product.attributedPrice
         cell.productStock.attributedText = product.attributedStock
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    private func getImage(from url: String) -> UIImage? {
+        guard let url = URL(string: url), let imageData = try? Data(contentsOf: url) else {
+            let defaultImage = UIImage(systemName: "xmark.icloud")
+            return defaultImage?.withTintColor(.systemGray)
+        }
+        return UIImage(data: imageData)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
