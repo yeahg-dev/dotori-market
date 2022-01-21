3e94dc42c843c95c9c2152561004aa4becafa295
yeahg-dev <yeahg.dev@gmail.com>
2022-01-21 16:04:19 +0900

    feat: ImageAddCollectionViewCell 스토리보드로 구현 후 IBOutlet 연결

---
+++ OpenMarket/OpenMarket/Cell/ImageAddCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageAddCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by lily on 2022/01/21.
+//
+
+import UIKit
+
+final class ImageAddCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet private weak var addedImageCountLabel: UILabel?
+}
