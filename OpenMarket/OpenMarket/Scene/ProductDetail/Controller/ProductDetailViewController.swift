36b20d1eaf70e32c5f81386d5b41fa74cc6ae027
yeahg-dev <yeahg.dev@gmail.com>
2022-06-08 23:31:24 +0900

    feat: ProductDetail뷰의 pageControl 코드로 구현

--- OpenMarket/OpenMarket/Scene/ProductDetail/Controller/ProductDetailViewController.swift
+++ OpenMarket/OpenMarket/Scene/ProductDetail/Controller/ProductDetailViewController.swift
@@ -15,11 +15,22 @@ final class ProductDetailViewController: UIViewController {
     @IBOutlet private weak var productSellingPrice: UILabel?
     @IBOutlet private weak var discountRate: UILabel?
     @IBOutlet private weak var productDescription: UITextView?
+    private let imagePageControl = UIPageControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.layoutImagePageControl()
+    }
+    
+    private func layoutImagePageControl() {
+        self.prductImageCollectionView?.addSubview(imagePageControl)
+        self.imagePageControl.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.imagePageControl.centerXAnchor.constraint(
+                equalTo: self.prductImageCollectionView!.centerXAnchor),
+            self.imagePageControl.bottomAnchor.constraint(
+                equalTo: self.prductImageCollectionView!.bottomAnchor)
+        ])
     }
 
 }
