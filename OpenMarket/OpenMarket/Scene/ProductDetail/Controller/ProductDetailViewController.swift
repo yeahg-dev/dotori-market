97b95a924bd6fb3d80bb6802b0b3df82ce7cb91f
yeahg-dev <yeahg.dev@gmail.com>
2022-06-09 17:41:36 +0900

    feat: API로 ProductDetail 다운로드 및 뷰 바인딩 구현

--- OpenMarket/OpenMarket/Scene/ProductDetail/Controller/ProductDetailViewController.swift
+++ OpenMarket/OpenMarket/Scene/ProductDetail/Controller/ProductDetailViewController.swift
@@ -19,20 +19,39 @@ final class ProductDetailViewController: UIViewController {
     
     // MARK: - UI Property
     private let imagePageControl = UIPageControl()
+    private let flowLayout = UICollectionViewFlowLayout()
     
     // MARK: - Property
+    private let apiService = APIExecutor()
     private var productID: Int?
     private var productDetail: ProductDetail?
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.layoutImagePageControl()
+        self.configureCollectionViewFlowLayout()
         self.configureNavigationItem()
+        self.layoutImagePageControl()
+        self.downloadProductDetail(prodcutID: productID)
+    }
+
+    private func configureCollectionViewFlowLayout() {
+        let cellWidth = self.view.frame.width
+        flowLayout.itemSize = CGSize(width: cellWidth, height: cellWidth)
+        flowLayout.minimumLineSpacing = 0
+        flowLayout.minimumInteritemSpacing = 0
+        flowLayout.scrollDirection = .horizontal
+        self.prductImageCollectionView?.collectionViewLayout = flowLayout
+    }
+    
+    private func configureNavigationItem() {
+        let composeButton = UIBarButtonItem(barButtonSystemItem: .compose, target: nil, action: nil)
+        self.navigationItem.setRightBarButton(composeButton, animated: true)
     }
     
     private func layoutImagePageControl() {
-        self.prductImageCollectionView?.addSubview(imagePageControl)
+        self.view.addSubview(imagePageControl)
+        self.prductImageCollectionView?.translatesAutoresizingMaskIntoConstraints = false
         self.imagePageControl.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             self.imagePageControl.centerXAnchor.constraint(
@@ -42,17 +61,74 @@ final class ProductDetailViewController: UIViewController {
         ])
     }
     
-    private func configureNavigationItem() {
-        let composeButton = UIBarButtonItem(barButtonSystemItem: .compose, target: nil, action: nil)
-        self.navigationItem.setRightBarButton(composeButton, animated: true)
-    }
-    
     private func configureNavigationTitle(with title: String) {
-        self.navigationController?.navigationItem.title = title
+        self.navigationItem.title = title
     }
 
     // MARK: - Method
     func setProduct(_ id: Int) {
         self.productID = id
     }
+    
+    private func downloadProductDetail(prodcutID: Int?) {
+        guard let id = prodcutID else { return }
+         
+        let request = ProductDetailRequest(productID: id)
+        apiService.execute(request) { [weak self] (result: Result<ProductDetail, Error>) in
+            switch result {
+            case .success(let product):
+                self?.productDetail = product
+                DispatchQueue.main.async {
+                    self?.updateProdutDetail(with: product)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func updateProdutDetail(with product: ProductDetail) {
+        self.fillUI(with: product)
+        self.prductImageCollectionView?.reloadData()
+    }
+    
+    private func fillUI(with product: ProductDetail) {
+        self.configureNavigationTitle(with: product.name)
+        self.productName?.text = product.name
+        self.productPrice?.text = product.price.description
+        self.productSellingPrice?.text = product.bargainPrice.description
+        self.productDescription?.text = "상품설명"
+        self.imagePageControl.numberOfPages = product.images.count
+    }
+    
 }
+
+extension ProductDetailViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return productDetail?.images.count ?? .zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        guard let cell = self.prductImageCollectionView?.dequeueReusableCell(withReuseIdentifier: "PrdouctDetailCollectionViewCell", for: indexPath) as? PrdouctDetailCollectionViewCell else {
+            return PrdouctDetailCollectionViewCell()
+        }
+        
+        if let imageURLString = self.productDetail?.images[indexPath.row].thumbnailURL,
+           let imageURL = URL(string: imageURLString) {
+            cell.fill(with: imageURL)
+        }
+
+        return cell
+    }
+    
+}
+
+extension ProductDetailViewController: UICollectionViewDelegate {
+    
+}
+
---
+++ OpenMarket/OpenMarket/Scene/ProductDetail/View/PrdouctDetailCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PrdouctDetailCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by 1 on 2022/06/09.
+//
+
+import UIKit
+
+class PrdouctDetailCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var prdouctImage: UIImageView!
+    
+    private let invalidImage = UIImage(systemName: "xmark.icloud.fill")
+    private var cancellableImageTask: Cancellable?
+    
+    override func prepareForReuse() {
+        self.prdouctImage?.image = nil
+        self.cancellableImageTask?.cancel()
+    }
+    
+    func fill(with imageURL: URL) {
+        self.cancellableImageTask = prdouctImage.setImage(with: imageURL, invalidImage: invalidImage!)
+    }
+    
+}
