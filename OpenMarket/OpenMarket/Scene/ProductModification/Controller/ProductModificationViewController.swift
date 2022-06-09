25138d96528747f3befdf3a82715e0f4a9e0ba92
yeahg-dev <yeahg.dev@gmail.com>
2022-06-09 23:19:21 +0900

    feat: productImageCollectionView dataSource메서드 구현

--- OpenMarket/OpenMarket/Scene/ProductModification/Controller/ProductModificationViewController.swift
+++ OpenMarket/OpenMarket/Scene/ProductModification/Controller/ProductModificationViewController.swift
@@ -21,12 +21,28 @@ class ProductModificationViewController: UIViewController {
     // MARK: - Property
     private let apiService = APIExecutor()
     private var productID: Int?
-    private var productDetail: ProductDetail?
+    private var cells: [CellType] = [.imagePickerCell]
+    private var productImages: [UIImage] = []
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureCollectionViewLayout()
         self.downloadProductDetail(of: productID)
+    }
+    
+    // MARK: - Layout
+    private func configureCollectionViewLayout() {
+        self.productImageCollectionView?.showsVerticalScrollIndicator = false
+        self.productImageCollectionView?.showsHorizontalScrollIndicator = false
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        
+        let cellWidth = view.bounds.size.width / 4
+        flowLayout.itemSize = CGSize(width: cellWidth, height: cellWidth)
+        flowLayout.minimumLineSpacing = 10
+        flowLayout.sectionInset = UIEdgeInsets(top: .zero, left: 10, bottom: .zero, right: 10)
+        self.productImageCollectionView?.collectionViewLayout = flowLayout
     }
     
     // MARK: - IBAction
@@ -46,9 +62,17 @@ class ProductModificationViewController: UIViewController {
         apiService.execute(request) { [weak self] (result: Result<ProductDetail, Error>) in
             switch result {
             case .success(let product):
-                self?.productDetail = product
                 DispatchQueue.main.async {
-                    self?.updateProdutDetail(wtih: product)
+                    self?.fillUI(wtih: product)
+                }
+                for image in product.images {
+                    self?.cells.append(.productImageCell)
+                    if let url = URL(string: image.thumbnailURL) {
+                        self?.downloadAndappendProductImage(of: url)
+                    }
+                }
+                DispatchQueue.main.async {
+                    self?.productImageCollectionView?.reloadData()
                 }
             case .failure(let error):
                 print(error.localizedDescription)
@@ -56,7 +80,7 @@ class ProductModificationViewController: UIViewController {
         }
     }
     
-    private func updateProdutDetail(wtih product: ProductDetail) {
+    private func fillUI(wtih product: ProductDetail) {
         self.productNameLabel?.text = product.name
         self.prdouctPriceLabel?.text = product.price.formatted()
         self.productDisconutPriceLabel?.text  = product.discountedPrice.formatted()
@@ -71,20 +95,52 @@ class ProductModificationViewController: UIViewController {
         }
     }
     
+    private func downloadAndappendProductImage(of url: URL) {
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            if let _ = error {
+                DispatchQueue.main.async {
+                    if let invalidImage = UIImage(systemName: "xmark.icloud.fill") {
+                        self?.productImages.append(invalidImage)
+                    }
+                }
+            } else {
+                DispatchQueue.main.async {
+                    guard let imageData = data,
+                          let uiimage = UIImage(data: imageData) else { return }
+                    self?.productImages.append(uiimage)
+                }
+            }
+        }
+        task.resume()
+    }
+
 }
 
 extension ProductModificationViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return cells.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        guard let cell = self.productImageCollectionView?.dequeueReusableCell(withReuseIdentifier: "ProductImageCollectionViewCell", for: indexPath) as? ProductImageCollectionViewCell else {
-            return ProductImageCollectionViewCell()
-        }
+        let cellType = cells[safe: indexPath.item]
         
-        return cell
+        switch cellType {
+        case .imagePickerCell:
+            let cell = collectionView.dequeueReusableCell(
+                withClass: ImagePickerCollectionViewCell.self,
+                for: indexPath
+            )
+            return cell
+        default:
+            let cell = collectionView.dequeueReusableCell(
+                withClass: ProductImageCollectionViewCell.self,
+                for: indexPath
+            )
+            let targetImage = productImages[safe: indexPath.item - 1]
+            cell.updateProductImageView(image: targetImage)
+            return cell
+        }
     }
 
 }
