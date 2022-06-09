8b8246e68427bb239aa3736de238b9da4c11c3e1
yeahg-dev <yeahg.dev@gmail.com>
2022-06-09 21:23:06 +0900

    feat: ProductModificationViewContorller 클래스 구현

---
+++ OpenMarket/OpenMarket/Scene/ProductModification/Controller/ProductModificationViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ProductModificationViewController.swift
+//  OpenMarket
+//
+//  Created by 1 on 2022/06/09.
+//
+
+import UIKit
+
+class ProductModificationViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var productImageCollectionView: UICollectionView?
+    @IBOutlet weak var productNameLabel: UITextField?
+    @IBOutlet weak var prdouctPriceLabel: UITextField?
+    @IBOutlet weak var productCurrencySegmentedControl: UISegmentedControl?
+    @IBOutlet weak var productDisconutPriceLabel: UITextField?
+    @IBOutlet weak var productStockLabel: UITextField?
+    @IBOutlet weak var productDescriptionTextView: UITextView?
+    
+    // MARK: - Property
+    private let apiService = APIExecutor()
+    private var productID: Int?
+    private var productDetail: ProductDetail?
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.downloadProductDetail(of: productID)
+    }
+    
+    // MARK: -Method
+    func setProduct(_ productID: Int) {
+        self.productID = productID
+    }
+    
+    private func downloadProductDetail(of prodcutID: Int?) {
+        guard let id = prodcutID else { return }
+        
+        let request = ProductDetailRequest(productID: id)
+        apiService.execute(request) { [weak self] (result: Result<ProductDetail, Error>) in
+            switch result {
+            case .success(let product):
+                self?.productDetail = product
+                DispatchQueue.main.async {
+                    self?.updateProdutDetail(wtih: product)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func updateProdutDetail(wtih product: ProductDetail) {
+        self.productNameLabel?.text = product.name
+        self.prdouctPriceLabel?.text = product.price.formatted()
+        self.productDisconutPriceLabel?.text  = product.discountedPrice.formatted()
+        self.productStockLabel?.text = product.stock.formatted()
+        self.productDescriptionTextView?.text = product.description
+        let currency = product.currency
+        switch currency {
+        case .krw:
+            self.productCurrencySegmentedControl?.selectedSegmentIndex = 0
+        case .usd:
+            self.productCurrencySegmentedControl?.selectedSegmentIndex = 1
+        }
+    }
+    
+}
