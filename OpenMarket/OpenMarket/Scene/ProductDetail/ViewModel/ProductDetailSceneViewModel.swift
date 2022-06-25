948f9e1486309b3506af46717e9328039e8e43c8
yeahg-dev <yeahg.dev@gmail.com>
2022-06-26 00:42:41 +0900

    refactor: ProductDetailSceneViewModel 구현 #11

---
+++ OpenMarket/OpenMarket/Scene/ProductDetail/ViewModel/ProductDetailSceneViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  ProductDetailSceneViewModel.swift
+//  OpenMarket
+//
+//  Created by 1 on 2022/06/25.
+//
+
+import Foundation
+import RxSwift
+
+final class ProductDetailSceneViewModel {
+    
+    private let APIService = MarketAPIService()
+    
+    struct Input {
+        let viewWillAppear: Observable<Int>
+    }
+    
+    struct Output {
+        let prdouctName: Observable<String>
+        let productImagesURL: Observable<[Image]> // pageControl
+        let productPrice: Observable<String?>
+        let prodcutSellingPrice: Observable<String>
+        let productDiscountedRate: Observable<String?>
+        let productStock: Observable<String>
+        let productDescription: Observable<String>
+    }
+    
+    func transform(input: Input) -> Output {
+        let productDetail = input.viewWillAppear
+            .map { productID in
+                ProductDetailRequest(productID: productID) }
+            .flatMap { request -> Observable<ProductDetail> in
+                self.APIService.requestRx(request) }
+            .map { productDetail in
+                ProductDetailViewModel(product: productDetail) }
+            .share(replay: 1)
+        
+        let productName = productDetail.map { $0.name }
+        let productPrice = productDetail.map { $0.price }
+        let productSellingPrice = productDetail.map { $0.sellingPrice }
+        let productDiscountedRate = productDetail.map { $0.discountedRate }
+        let productStock = productDetail.map { $0.stock }
+        let prodcutDescription = productDetail.map { $0.description }
+        let productImages = productDetail.map { $0.images }
+        
+        return Output(prdouctName: productName,
+                      productImagesURL: productImages,
+                      productPrice: productPrice,
+                      prodcutSellingPrice: productSellingPrice,
+                      productDiscountedRate: productDiscountedRate,
+                      productStock: productStock,
+                      productDescription: prodcutDescription)
+    }
+}
