fc43b20b4774cf2183efd1c2aec17f9308ed44b3
yeahg-dev <yeahg.dev@gmail.com>
2022-07-22 22:28:30 +0900

    feat: ProductRegisterationRecorder 구현 #6

--- DotoriMarket/DotoriMarket/Domain/Model/RegisterdProduct.swift
+++ DotoriMarket/DotoriMarket/Domain/Model/RegisterdProduct.swift
@@ -1,5 +1,5 @@
 //
-//  MyProduct.swift
+//  RegisterdProduct.swift
 //  DotoriMarket
 //
 //  Created by 1 on 2022/07/22.
@@ -9,7 +9,7 @@ import Foundation
 
 import RealmSwift
 
-class MyProduct: Object {
+class RegisterdProduct: Object {
     
     @objc dynamic var id: Int64 = 0
     @objc dynamic var registerationDate: Date = Date()
---
+++ DotoriMarket/DotoriMarket/Domain/ProductManage/ProductRegisterationRecorder.swift
@@ -0,0 +1,31 @@
+//
+//  ProductRegisterationRecorder.swift
+//  DotoriMarket
+//
+//  Created by 1 on 2022/07/22.
+//
+
+import Foundation
+
+import RealmSwift
+
+struct ProductRegisterationRecorder {
+    
+    let realm = try! Realm()
+    
+    func recordProductRegistraion(productID: Int) {
+        let productToRegister = RegisterdProduct()
+        productToRegister.id = Int64(productID)
+        
+        try? realm.write {
+            realm.add(productToRegister)
+        }
+    }
+    
+    func readRegisterdProductIDs() -> [Int] {
+        let products = realm.objects(RegisterdProduct.self)
+
+        return Array(products).compactMap{ $0.id }.map{ Int($0) }
+    }
+    
+}
