9b4004622e40a1a14f0227d1a47d959f26e84d82
yeahg-dev <yeahg.dev@gmail.com>
2022-07-26 16:23:30 +0900

    feat: ProudctRepository 프로토콜 정의

---
+++ DotoriMarket/DotoriMarket/Repository/Protocol/ProductRepository.swift
@@ -0,0 +1,18 @@
+//
+//  ProductRepository.swift
+//  DotoriMarket
+//
+//  Created by 1 on 2022/07/26.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol ProductRepository {
+    
+    func fetchProductListPage(of page: Int) -> Observable<ProductListPage>
+    
+    func fetchProductDetail(of productID: Int) -> Observable<ProductDetail>
+
+}
