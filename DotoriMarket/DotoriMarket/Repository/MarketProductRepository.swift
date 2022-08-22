b0fc98bfef77c4b317b622b8bb7495c3fc7a5cfa
yeahg-dev <yeahg.dev@gmail.com>
2022-08-22 22:47:36 +0900

    refactor: 응답 파싱 실패시 에러를 반환 사용자가 알도록 개선

--- DotoriMarket/DotoriMarket/Repository/MarketProductRepository.swift
+++ DotoriMarket/DotoriMarket/Repository/MarketProductRepository.swift
@@ -20,10 +20,10 @@ struct MarketProductRepository: ProductRepository {
     func fetchProductListPage(of page: Int, itemsPerPage: Int) -> Observable<ProductListPage> {
         let request = ProductsListPageRequest(pageNo: page, itemsPerPage: itemsPerPage)
         return self.service.requestRx(request)
-            .map{ data -> ProductsListPageResponse? in
-                guard let response: ProductsListPageResponse = JSONCodable().decode(from: data) else { return nil }
+            .map{ data -> ProductsListPageResponse in
+                guard let response: ProductsListPageResponse = JSONCodable().decode(from: data) else {
+                    throw MarketProductRepositorError.parsingFail }
                 return response }
-            .filterNil()
             .map{ $0.toDomain() }
     }
     
@@ -31,29 +31,34 @@ struct MarketProductRepository: ProductRepository {
         let request = ProductDetailRequest(productID: productID)
            
         return self.service.requestRx(request)
-            .map{ data -> ProductDetailResponse? in
-                guard let response: ProductDetailResponse = JSONCodable().decode(from: data) else { return nil }
+            .map{ data -> ProductDetailResponse in
+                guard let response: ProductDetailResponse = JSONCodable().decode(from: data) else {
+                    throw MarketProductRepositorError.parsingFail }
                 return response }
-            .filterNil()
             .map{ $0.toDomain() }
     }
 
     func requestProductEdit(with request: ProductEditRequest) -> Observable<ProductDetail> {
         return self.service.requestRx(request)
-            .map{ data -> ProductDetailResponse? in
-            guard let response: ProductDetailResponse = JSONCodable().decode(from: data) else { return nil }
+            .map{ data -> ProductDetailResponse in
+            guard let response: ProductDetailResponse = JSONCodable().decode(from: data) else {
+                throw MarketProductRepositorError.parsingFail }
             return response }
-            .filterNil()
             .map{ $0.toDomain() }
     }
     
     func requestProductRegistration(with request: ProductRegistrationRequest) -> Observable<ProductDetail> {
         return self.service.requestRx(request)
-            .map{ data -> ProductDetailResponse? in
-            guard let response: ProductDetailResponse = JSONCodable().decode(from: data) else { return nil }
+            .map{ data -> ProductDetailResponse in
+            guard let response: ProductDetailResponse = JSONCodable().decode(from: data) else {
+                throw MarketProductRepositorError.parsingFail }
             return response }
-            .filterNil()
             .map{ $0.toDomain() }
+    }
+    
+    enum MarketProductRepositorError: Error {
+        
+        case parsingFail
     }
     
 }
