b0661aec2c2a286a65bb26ff9e81b70c5c0cba82
yeahg-dev <yeahg.dev@gmail.com>
2023-03-01 17:16:02 +0900

    style: 코드 컨벤션에 맞추어 인텐트 수정

--- DotoriMarket/DotoriMarket/Presentation/Scene/ProductList/ViewModel/ProductListSceneViewModel.swift
+++ DotoriMarket/DotoriMarket/Presentation/Scene/ProductList/ViewModel/ProductListSceneViewModel.swift
@@ -56,37 +56,37 @@ final class ProductListSceneViewModel {
             .do(onNext: { self.resetPage()
                 willStartLoadingIndicator.onNext(()) })
                 
-                let willLoadNextPage = input.willDisplayCellAtIndex
-                .map{ $0.row }
-                .filter{ currentRow in
-                    (currentRow == self.productsViewModels.count - self.paginationBuffer) && self.hasNextPage }
-                .map{ _ in }
-                .do(onNext: { willStartLoadingIndicator.onNext(()) })
-                    
-                    let listViewDidStartRefresh = input.listViewDidStartRefresh
-                    .do(onNext: { self.resetPage() })
+        let willLoadNextPage = input.willDisplayCellAtIndex
+            .map{ $0.row }
+            .filter{ currentRow in
+            (currentRow == self.productsViewModels.count - self.paginationBuffer) && self.hasNextPage }
+            .map{ _ in }
+            .do(onNext: { willStartLoadingIndicator.onNext(()) })
+            
+        let listViewDidStartRefresh = input.listViewDidStartRefresh
+            .do(onNext: { self.resetPage() })
                         
-                        let networkErrorAlert = PublishSubject<AlertViewModel>()
+        let networkErrorAlert = PublishSubject<AlertViewModel>()
                         
-                        let products = Observable.merge(
-                            viewWillAppear, willLoadNextPage, listViewDidStartRefresh)
-                        .observe(on: ConcurrentDispatchQueueScheduler.init(qos: .default))
-                        .flatMap{ _ -> Observable<([ProductViewModel], Bool)> in
-                            self.productListUsecase.fetchPrdoucts(
-                                pageNo: self.currentPage + 1,
-                                itemsPerPage: 20,
-                                searchValue: nil) }
-                        .do(onError: { _ in
-                            networkErrorAlert.onNext(NetworkErrorAlertViewModel() as AlertViewModel)
-                            willEndLoadingIndicator.onNext(()) })
-                            .retry(when: { _ in listViewDidStartRefresh })
-                            .do(onNext: { (viewModels, hasNextPage) in
-                                self.currentPage += 1
-                                self.hasNextPage = hasNextPage
-                                self.productsViewModels.append(contentsOf: viewModels)
-                                willEndLoadingIndicator.onNext(()) })
-                                .map{ _ in self.productsViewModels}
-                                .asDriver(onErrorJustReturn: Array<ProductViewModel>())
+        let products = Observable.merge(
+            viewWillAppear, willLoadNextPage, listViewDidStartRefresh)
+            .observe(on: ConcurrentDispatchQueueScheduler.init(qos: .default))
+            .flatMap{ _ -> Observable<([ProductViewModel], Bool)> in
+            self.productListUsecase.fetchPrdoucts(
+                pageNo: self.currentPage + 1,
+                itemsPerPage: 20,
+                searchValue: nil) }
+            .do(onError: { _ in
+                networkErrorAlert.onNext(NetworkErrorAlertViewModel() as AlertViewModel)
+                willEndLoadingIndicator.onNext(()) })
+            .retry(when: { _ in listViewDidStartRefresh })
+            .do(onNext: { (viewModels, hasNextPage) in
+                self.currentPage += 1
+                self.hasNextPage = hasNextPage
+                self.productsViewModels.append(contentsOf: viewModels)
+                willEndLoadingIndicator.onNext(()) })
+            .map{ _ in self.productsViewModels}
+            .asDriver(onErrorJustReturn: Array<ProductViewModel>())
         
         let endRefresh = products.map { _ in }
             .asDriver(onErrorJustReturn: ())
@@ -94,14 +94,17 @@ final class ProductListSceneViewModel {
         let pushProductDetailView = input.cellDidSelectedAt
             .map{ $0.row }
             .map{ index -> Int in
-                guard let product = self.productsViewModels[safe: index] else { return .zero }
+                guard let product = self.productsViewModels[safe: index] else {
+                    return .zero }
                 return product.id }
             .asDriver(onErrorJustReturn: 0)
         
         return Output(
             navigationBarComponent: navigationBarComponent,
-            willStartLoadingIndicator: willStartLoadingIndicator.asDriver(onErrorJustReturn: ()),
-            willEndLoadingIndicator: willEndLoadingIndicator.asDriver(onErrorJustReturn: ()),
+            willStartLoadingIndicator: willStartLoadingIndicator.asDriver(
+                onErrorJustReturn: ()),
+            willEndLoadingIndicator: willEndLoadingIndicator.asDriver(
+                onErrorJustReturn: ()),
             products: products,
             listViewWillEndRefresh: endRefresh,
             pushProductDetailView: pushProductDetailView,
